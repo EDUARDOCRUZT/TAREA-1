9ac33f759752a3cbe561d178a125a2c6356cbd39
EDUARDOCRUZT <55502300+EDUARDOCRUZT@users.noreply.github.com>
2019-10-15 16:13:13 -0500

    Update E10.cpp

--- E10.cpp
+++ E10.cpp
@@ -6,7 +6,10 @@ int main(){
 float l,p;
 printf("INGRESE LA CANTIDAD DE LITROS CONSUMIDOS:_");
 scanf("%f",&l);
-if(l>50){
+if(l<=50){
+printf("EL MONTO A PAGAR ES DE: $0.00")
+}
+if(l>70){
     if(l<200){
     p=l*10;
     printf("EL MONTO A PAGAR ES DE:$ %f",p);
@@ -16,7 +19,7 @@ if(l>=200){
     p=l*20;
     printf("EL MONTO A PAGAR ES DE:$ %f",p);
 }
-else {
+if(l<=70,l>50) {
 printf("EL MONTO A PAGAR ES DE:$200.00");
 }
 }
